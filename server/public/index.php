acba11e8c8d5eb164a50390fff008d6e8acf25b3
tcormons <tcormons@gmail.com>
2019-11-25 19:21:16 +0000

    template upload

---
+++ server/public/index.php
@@ -0,0 +1,9 @@
+
+<?php
+require_once '../api/_lifecycle.php';
+switch ($request['path']) {
+  case '/api/health-check':
+    require_once "..${request['path']}.php";
+  default:
+    throw new ApiError("Cannot ${request['method']} ${request['path']}", 404);
+}
