6cd8503e08b0d71c4fb4fe97cb47c608867dede6
tcormons <tcormons@gmail.com>
2019-11-28 01:31:15 +0000

    testing end point works for /api/search

--- server/public/index.php
+++ server/public/index.php
@@ -3,6 +3,7 @@ require_once '../api/_lifecycle.php';
 switch ($request['path']) {
   case '/api/test':
   case '/api/health-check':
+  case '/api/search':
     require_once "..${request['path']}.php";
   default:
     throw new ApiError("Cannot ${request['method']} ${request['path']}", 404);
