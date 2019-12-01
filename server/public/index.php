e77bdee759366f5e90ea22b56caa95b591bfa0f2
Kris <kmringler@gmail.com>
2019-12-01 23:18:01 +0000

    end points for event search, location search, and sport search all work properly, still need to add prepared statements to each query

--- server/public/index.php
+++ server/public/index.php
@@ -3,7 +3,9 @@ require_once '../api/_lifecycle.php';
 switch ($request['path']) {
   case '/api/test':
   case '/api/health-check':
-  case '/api/search':
+  case '/api/sport-search':
+  case '/api/event-search':
+  case '/api/location-search':
     require_once "..${request['path']}.php";
   default:
     throw new ApiError("Cannot ${request['method']} ${request['path']}", 404);
