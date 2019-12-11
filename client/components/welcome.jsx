7b08a74b907db48b413915f209be844b68713e81
tcormons <tcormons@gmail.com>
2019-12-11 19:02:55 +0000

    fixed bug in display event from liked list

--- client/components/welcome.jsx
+++ client/components/welcome.jsx
@@ -15,18 +15,18 @@ function Welcome() {
         Filter by your sport of choice, day of choice, and location of choice, then go out and play!
       </h4>
       </div>
-      <div className='button mt-2 headers-font-ubuntu'>
+      <div className='button mt-2 headers-font-ubuntu col-12'>
         <Link to='/signIn' >
           <button className='btn btn-primary block submit-button'>Sign In</button>
         </Link>
         <Link to= '/createAccount' >
           <button className='btn btn-primary block submit-button mt-3'>Create an Account</button>
         </Link>
-        <Link to= '/search' >
+        {/* <Link to= '/search' >
           <button className='btn btn-primary block mt-2 skip-button'>Skip
             <i className="fas fa-chevron-right ml-2"></i>
           </button>
-        </Link>
+        </Link> */}
 
       </div>
     </div>
