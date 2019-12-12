76d8da6693e87660659336f759d0fae44f0ed311
Kianayumi <kianayumi@gmail.com>
2019-12-12 00:13:51 +0000

    testing phone interface

--- client/components/review-list-item.jsx
+++ client/components/review-list-item.jsx
@@ -7,15 +7,15 @@ class ReviewListItem extends React.Component {
       this.props.onSpecificReviewClick(this.props.id);
     };
     return (
-      <div className="reviewListItem mb-2 d-flex flex-column"
+      <div className="reviewListItem mb-2 p-2 d-flex flex-column"
         onClick={handleClick}>
-        <div className="d-flex justify-content-between mt-2 p-2">
-          <div className="pl-2">{this.props.userName}</div>
-          <div className="rating mr-2" style={{ backgroundImage: `url(${this.props.ratingImage})` }}></div>
+        <div className="d-flex justify-content-between mt-2 pl-2 pr-2">
+          <div className="">{this.props.userName}</div>
+          <div className="rating" style={{ backgroundImage: `url(${this.props.ratingImage})` }}></div>
         </div>
-        <blockquote className="blockquote">
-          <p className="pl-2 mb-0"><i>{this.props.reviewDescription}</i></p>
-        </blockquote>
+        <div className="d-flex justify-content-between m-2">
+          <i>{this.props.reviewDescription}</i>
+        </div>
         {/* <div className=""><i>{this.props.reviewDescription}</i></div> */}
       </div>
     );
